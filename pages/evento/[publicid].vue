7523a870d32bce13f2418b17ff9841ff8655e9bc
Rômulo Augusto <romuloffall@gmail.com>
2024-09-27 20:52:03 -0300

    #implement pages and api routes

---
+++ pages/evento/[publicid].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    teste
+
+    <p>publicid: {{ publicid }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+  const route = useRoute();
+
+  const publicid = ref(route.params.publicid);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/evento/admin/[adminid].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    teste
+    <p>adminid: {{ adminid }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+  const route = useRoute();
+
+  const adminid = ref(route.params.adminid);
+</script>
