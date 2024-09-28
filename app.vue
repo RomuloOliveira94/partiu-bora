e619ae85d321c693e48a27a62c0b011c778a0e20
Rômulo Augusto <romuloffall@gmail.com>
2024-09-27 22:03:54 -0300

    #add layout

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <Header />
+    <NuxtLayout>
+      <div class="container mx-auto p-8">
+        <NuxtPage />
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
