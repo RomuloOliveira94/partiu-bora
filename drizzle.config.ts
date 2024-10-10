3239c1fccf0ea9664a1956e519bbaa492197c239
Rômulo Augusto <romuloffall@gmail.com>
2024-10-10 14:03:35 -0300

    #clear

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,30 +1,3 @@
-/* import { defineConfig } from "drizzle-kit";
-let configuration;
-
-//if (process.env.NODE_ENV === "production") {
-configuration = defineConfig({
-  schema: "./server/db/schema.ts",
-  out: "./drizzle",
-  dialect: "sqlite",
-  driver: "turso",
-  dbCredentials: {
-    url: process.env.TURSO_DB_URL!,
-    authToken: process.env.TURSO_DB_TOKEN!,
-  },
-});
-// } else {
-//   configuration = defineConfig({
-//     schema: "./server/db/schema.ts",
-//     out: "./drizzle",
-//     dialect: "sqlite",
-//     dbCredentials: {
-//       url: "./server/db/sqlite.db",
-//     },
-//   });
-// }
-
-export default configuration; */
-
 import type { Config } from "drizzle-kit";
 
 export default {
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,27 +1,3 @@
-// // config({ path: ".env" });
-// /* import { drizzle } from "drizzle-orm/better-sqlite3";
-// import Database from "better-sqlite3"; */
-// import { LibSQLDatabase, drizzle as prodDrizzle } from "drizzle-orm/libsql";
-// import { createClient } from "@libsql/client";
-
-// const { tursoConnectionUrl, tursoAuthToken } = useRuntimeConfig();
-
-// let sqlite;
-// let database: LibSQLDatabase | null = null;
-
-// // if (config.environment === "production") {
-// sqlite = createClient({
-//   url: tursoConnectionUrl,
-//   authToken: tursoAuthToken,
-// });
-// db = prodDrizzle(sqlite);
-// // } else {
-// //   sqlite = new Database("./server/db/sqlite.db");
-// //   db = drizzle(sqlite);
-// // }
-
-// export default db;
-
 import { createClient } from "@libsql/client/http";
 import { drizzle, type LibSQLDatabase } from "drizzle-orm/libsql";
 
