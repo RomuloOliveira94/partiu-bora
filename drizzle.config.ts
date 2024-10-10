0ef26d639f5fb16caa4ab1364315b2aebf6169c6
Rômulo Augusto <romuloffall@gmail.com>
2024-10-10 13:19:25 -0300

    #fix database config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,26 +5,26 @@ config({ path: ".env" });
 
 let configuration;
 
-if (process.env.NUXT_ENVIRONMENT === "production") {
-  configuration = defineConfig({
-    schema: "./server/db/schema.ts",
-    out: "./drizzle",
-    dialect: "sqlite",
-    driver: "turso",
-    dbCredentials: {
-      url: process.env.NUXT_TURSO_CONNECTION_URL || "",
-      authToken: process.env.NUXT_TURSO_AUTH_TOKEN || "",
-    },
-  });
-} else {
-  configuration = defineConfig({
-    schema: "./server/db/schema.ts",
-    out: "./drizzle",
-    dialect: "sqlite",
-    dbCredentials: {
-      url: "./server/db/sqlite.db",
-    },
-  });
-}
+//if (process.env.NODE_ENV === "production") {
+configuration = defineConfig({
+  schema: "./server/db/schema.ts",
+  out: "./drizzle",
+  dialect: "sqlite",
+  driver: "turso",
+  dbCredentials: {
+    url: process.env.NUXT_TURSO_CONNECTION_URL || "",
+    authToken: process.env.NUXT_TURSO_AUTH_TOKEN || "",
+  },
+});
+// } else {
+//   configuration = defineConfig({
+//     schema: "./server/db/schema.ts",
+//     out: "./drizzle",
+//     dialect: "sqlite",
+//     dbCredentials: {
+//       url: "./server/db/sqlite.db",
+//     },
+//   });
+// }
 
 export default configuration;
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,27 +1,24 @@
 // import { config } from "dotenv";
 // config({ path: ".env" });
-import { SqliteRemoteDatabase } from "drizzle-orm/sqlite-proxy";
-import { BaseSQLiteDatabase } from "drizzle-orm/sqlite-core";
-
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
+/* import { drizzle } from "drizzle-orm/better-sqlite3";
+import Database from "better-sqlite3"; */
 
 import { drizzle as prodDrizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
 const config = useRuntimeConfig();
 let sqlite;
-let db: any; 
+let db: any;
 
-if (config.environment === "production") {
-  sqlite = createClient({
-    url: config.tursoConnectionUrl,
-    authToken: config.tursoAuthToken,
-  });
-  db = prodDrizzle(sqlite);
-} else {
-  sqlite = new Database("./server/db/sqlite.db");
-  db = drizzle(sqlite);
-}
+// if (config.environment === "production") {
+sqlite = createClient({
+  url: config.tursoConnectionUrl,
+  authToken: config.tursoAuthToken,
+});
+db = prodDrizzle(sqlite);
+// } else {
+//   sqlite = new Database("./server/db/sqlite.db");
+//   db = drizzle(sqlite);
+// }
 
 export default db;
