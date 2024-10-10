b1ede07cdced6b24635d17fc793d6d2a1a8f8795
Rômulo Augusto <romuloffall@gmail.com>
2024-10-10 13:49:40 -0300

    #fix db type

--- server/db/index.ts
+++ server/db/index.ts
@@ -25,17 +25,15 @@
 import { createClient } from "@libsql/client/http";
 import { drizzle, type LibSQLDatabase } from "drizzle-orm/libsql";
 
-let db: LibSQLDatabase | null = null;
+let db: LibSQLDatabase;
 
 const { tursoConnectionUrl, tursoAuthToken } = useRuntimeConfig();
 
-if (tursoAuthToken && tursoConnectionUrl) {
-  db = drizzle(
-    createClient({
-      url: tursoConnectionUrl,
-      authToken: tursoAuthToken,
-    })
-  );
-}
+db = drizzle(
+  createClient({
+    url: tursoConnectionUrl,
+    authToken: tursoAuthToken,
+  })
+);
 
 export default db;
